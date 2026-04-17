a18820442ee76de9ec4a38f05bbfef034f8570d5
Senadhi Mandina <haksenadhimandina123@gmail.com>
2026-04-17 11:58:21 +0530

    Initial SmartCampusApi coursework
    Restful Smart Campus API built with JAX-RS for managing rooms, sensors and historical sensor readings.

---
+++ src/main/java/com/senadhi/smartcampusapi/JAXRSConfiguration.java
@@ -0,0 +1,12 @@
+package com.senadhi.smartcampusapi;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/api/v1")
+public class JAXRSConfiguration extends ResourceConfig {
+
+    public JAXRSConfiguration() {
+        packages("com.senadhi.smartcampusapi");
+    }
+}
